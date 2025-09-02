857a84c1342473eaa258461384f00362931f0bab
Nuhan <nuhan@bdCallings-Mac-mini-70.local>
2025-09-02 08:39:53 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/quikle_user/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.quikle_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:quikle_user/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
