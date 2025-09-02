20c37e38aae75c9e8bb2b8d19b402e94cbaa4631
Mohammed Maruf Islam <marufislam74208@gmail.com>
2025-09-02 17:26:13 +0600

    Did the splash screen including the video

--- lib/core/bindings/controller_binder.dart
+++ lib/core/bindings/controller_binder.dart
@@ -1,6 +1,5 @@
-
-
 import 'package:get/get.dart';
+import 'package:quikle_user/features/splash/controllers/splash_controller.dart';
 
 class ControllerBinder extends Bindings {
   @override
@@ -9,6 +8,7 @@ class ControllerBinder extends Bindings {
     //       () => LogInController(),
     //   fenix: true,
     // );
-
+    Get.lazyPut<SplashController>(() => SplashController(), fenix: true);
+    //Get.lazyPut<LoginController>(() => LoginController(), fenix: true);
   }
 }
