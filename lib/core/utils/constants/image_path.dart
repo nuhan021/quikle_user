f204e877a44229148e5c9d92c959ee92ae36355a
Mohammed Maruf Islam <marufislam74208@gmail.com>
2025-10-04 16:55:58 +0600

    Fixed navbar

--- lib/core/utils/constants/image_path.dart
+++ lib/core/utils/constants/image_path.dart
@@ -1,6 +1,8 @@
 class ImagePath {
   static const String logo = 'assets/images/logo.png';
   static const String verifyIcon = 'assets/icons/verify.png';
+  static const String homeNav = 'assets/icons/homeIcon.png';
+  static const orderIcon = 'assets/icons/orderIcon.png';
   static const String categoryIcon = 'assets/icons/category.png';
   static const String profileIcon = 'assets/icons/profileImage.png';
   static const String profile = 'assets/icons/profile.png';
