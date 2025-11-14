5ea60e84b2467feb071d4092b26b93c2bb57d27c
MdMaruf-22 <marufislam74208@gmail.com>
2025-11-14 17:12:52 +0600

    login functionlity has been completed fully

--- lib/features/splash/controllers/splash_controller.dart
+++ lib/features/splash/controllers/splash_controller.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 import 'package:get/get.dart';
 import 'package:quikle_user/core/common/widgets/no_internet_screen.dart';
+import 'package:quikle_user/core/services/storage_service.dart';
 import 'package:quikle_user/core/utils/logging/logger.dart';
+import 'package:quikle_user/features/auth/presentation/screens/splash_wrapper.dart';
+import 'package:quikle_user/routes/app_routes.dart';
 import 'package:video_player/video_player.dart';
 import '../../../core/services/network_controller.dart';
 import '../../auth/presentation/screens/login_screen.dart';
@@ -64,10 +67,21 @@ class SplashController extends GetxController {
         'Checking network status from SplashController: ${networkController.hasConnection.value}',
       );
 
-      if (networkController.hasConnection.value) {
-        Get.off(() => const LoginScreen());
-      } else {
+      if (!networkController.hasConnection.value) {
         Get.off(() => const NoInternetScreen());
+        return;
+      }
+
+      // Check if user is already logged in
+      final bool hasToken = StorageService.hasToken();
+      AppLoggerHelper.debug('User has token: $hasToken');
+
+      if (hasToken) {
+        // User is logged in, navigate to home screen
+        Get.offAll(() => const SplashWrapper());
+      } else {
+        // User is not logged in, navigate to login screen
+        Get.off(() => const LoginScreen());
       }
     }
   }
