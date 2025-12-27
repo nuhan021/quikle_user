a15d7281ad7b4254cfbdcabe937cbfb936368f8b
GulamRosul <gulamrosul037@gmail.com>
2025-12-27 09:20:41 +0600

    Coupon screen fixes

--- lib/features/payout/presentation/widgets/coupon_section.dart
+++ lib/features/payout/presentation/widgets/coupon_section.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:quikle_user/core/common/styles/global_text_style.dart';
+import 'package:quikle_user/core/utils/constants/colors.dart';
 import 'package:quikle_user/core/utils/constants/enums/font_enum.dart';
 import '../../controllers/payout_controller.dart';
 
@@ -15,14 +16,57 @@ class CouponSection extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          'Apply Coupon Code',
-          style: getTextStyle(
-            font: CustomFonts.obviously,
-            fontSize: 16.sp,
-            fontWeight: FontWeight.w600,
-            color: const Color(0xFF333333),
-          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Apply Coupon Code',
+              style: getTextStyle(
+                font: CustomFonts.obviously,
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w600,
+                color: const Color(0xFF333333),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                payoutController.viewAllCoupons();
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 14.w, vertical: 10.h),
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    colors: [Color(0xFF26A969), Color(0xFF1EA254)],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  borderRadius: BorderRadius.circular(10.r),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.12),
+                      blurRadius: 6,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.local_offer, color: Colors.white, size: 12.sp),
+                    SizedBox(width: 8.w),
+                    Text(
+                      'View all',
+                      style: getTextStyle(
+                        font: CustomFonts.manrope,
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
         SizedBox(height: 8.h),
         const Divider(height: 1, thickness: 1, color: Color(0xFFE8E9E9)),
@@ -30,148 +74,80 @@ class CouponSection extends StatelessWidget {
 
         Obx(() {
           final applied = payoutController.appliedCoupon;
-          if (applied != null && applied['isValid'] == true) {
-            return _buildAppliedCoupon(payoutController);
-          }
-          return _buildCouponInput(payoutController);
-        }),
-      ],
-    );
-  }
-
-  Widget _buildCouponInput(PayoutController controller) {
-    return Container(
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.04),
-            blurRadius: 16,
-            offset: const Offset(0, 4),
-          ),
-        ],
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: TextFormField(
-              controller: controller.couponController,
-              textInputAction: TextInputAction.done,
-              onChanged: (_) {},
-              onFieldSubmitted: (_) {
-                if (controller.canApplyCoupon) controller.applyCoupon();
-              },
-              decoration: InputDecoration(
-                hintText: 'Type your Coupon Code',
-                hintStyle: getTextStyle(
-                  font: CustomFonts.inter,
-                  fontSize: 14.sp,
-                  color: const Color(0xFF7C7C7C),
-                ),
-                contentPadding: EdgeInsets.symmetric(
-                  horizontal: 12.w,
-                  vertical: 14.h,
-                ),
-                filled: true,
-                fillColor: Colors.white,
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.r),
-                  borderSide: const BorderSide(color: Color(0xFFB8B8B8)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.r),
-                  borderSide: const BorderSide(color: Color(0xFFB8B8B8)),
-                ),
-              ),
-              style: getTextStyle(
-                font: CustomFonts.inter,
-                fontSize: 14.sp,
-                color: const Color(0xFF1C1C1C),
-              ),
-            ),
-          ),
-          SizedBox(width: 8.w),
-
-          Obx(() {
-            final disabled = !controller.canApplyCoupon;
-            return GestureDetector(
-              onTap: disabled ? null : controller.applyCoupon,
-              child: AnimatedOpacity(
-                duration: const Duration(milliseconds: 120),
-                opacity: disabled ? 0.5 : 1,
-                child: Container(
-                  height: 48.h,
-                  padding: EdgeInsets.symmetric(horizontal: 20.w),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(8.r),
-                  ),
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Apply',
-                    style: getTextStyle(
-                      font: CustomFonts.manrope,
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w600,
-                      color: const Color(0xFFF8F8F8),
-                    ),
-                  ),
-                ),
-              ),
-            );
-          }),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildAppliedCoupon(PayoutController controller) {
-    final coupon = controller.appliedCoupon!;
-    return Container(
-      padding: EdgeInsets.all(12.w),
-      decoration: BoxDecoration(
-        color: const Color(0xFF26A969).withValues(alpha: 0.10),
-        border: Border.all(color: const Color(0xFF26A969)),
-        borderRadius: BorderRadius.circular(8.r),
-      ),
-      child: Row(
-        children: [
-          Icon(Icons.check_circle, color: const Color(0xFF26A969), size: 20.sp),
-          SizedBox(width: 12.w),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Coupon Applied: ${controller.couponController.text}',
-                  style: getTextStyle(
-                    font: CustomFonts.inter,
-                    fontSize: 14.sp,
-                    fontWeight: FontWeight.w600,
-                    color: const Color(0xFF26A969),
-                  ),
-                ),
-                SizedBox(height: 2.h),
-                Text(
-                  coupon['message'] ?? '',
-                  style: getTextStyle(
-                    font: CustomFonts.inter,
-                    fontSize: 12.sp,
-                    color: const Color(0xFF26A969),
-                  ),
+          return Container(
+            padding: EdgeInsets.all(12.w),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8.r),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withValues(alpha: 0.02),
+                  blurRadius: 8,
+                  offset: const Offset(0, 2),
                 ),
               ],
             ),
-          ),
-          GestureDetector(
-            onTap: controller.removeCoupon,
-            child: Icon(
-              Icons.close,
-              color: const Color(0xFF9E9E9E),
-              size: 20.sp,
+            child: Row(
+              children: [
+                Expanded(
+                  child: applied != null && applied['isValid'] == true
+                      ? Row(
+                          children: [
+                            Icon(
+                              Icons.check_circle,
+                              color: const Color(0xFF26A969),
+                              size: 18.sp,
+                            ),
+                            SizedBox(width: 10.w),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Coupon Applied',
+                                        style: getTextStyle(
+                                          font: CustomFonts.manrope,
+                                          fontSize: 14.sp,
+                                          fontWeight: FontWeight.w600,
+                                          color: AppColors.ebonyBlack,
+                                        ),
+                                      ),
+
+                                      Text(
+                                        '${payoutController.couponController.text}',
+                                        style: getTextStyle(
+                                          font: CustomFonts.inter,
+                                          fontSize: 14.sp,
+                                          fontWeight: FontWeight.w600,
+                                          color: const Color(0xFF26A969),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )
+                      : Text(
+                          'No coupon applied',
+                          style: getTextStyle(
+                            font: CustomFonts.inter,
+                            fontSize: 14.sp,
+                            color: const Color(0xFF333333),
+                          ),
+                        ),
+                ),
+                SizedBox(width: 12.w),
+              ],
             ),
-          ),
-        ],
-      ),
+          );
+        }),
+      ],
     );
   }
 }
